f14b302e3145dcdf115ad98c2cb7c701de0db7a7
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2021-09-27 12:16:37 +0200

    Bluetooth: Mesh: Set DFD phase to Applying Update upon apply request
    This commit sets DFD to Applying Update phase after receiving
    Distribution Apply message.
    
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

--- subsys/bluetooth/mesh/dfd_srv.c
+++ subsys/bluetooth/mesh/dfd_srv.c
@@ -420,6 +420,7 @@ static int handle_apply(struct bt_mesh_model *mod, struct bt_mesh_msg_ctx *ctx,
 		return 0;
 	}
 
+	srv->phase = BT_MESH_DFD_PHASE_APPLYING_UPDATE;
 	status_rsp(srv, ctx, BT_MESH_DFD_SUCCESS);
 
 	return 0;
