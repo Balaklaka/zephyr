7df7dee173c5b21d80f987522c446d9cf0bb2e9e
Anders Storrø <anders.storro@nordicsemi.no>
2022-04-01 14:49:36 +0200

    Tests: Bluetooth: Mesh: BLOB cli mod ret caps test
    Add bsim test for retrieve capabilities of the BLOB client model.
    
    Signed-off-by: Anders Storrø <anders.storro@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_mesh/src/main.c
+++ tests/bluetooth/bsim_bt/bsim_test_mesh/src/main.c
@@ -25,6 +25,7 @@ extern struct bst_test_list *test_heartbeat_install(struct bst_test_list *test);
 extern struct bst_test_list *test_access_install(struct bst_test_list *test);
 extern struct bst_test_list *test_ivi_install(struct bst_test_list *test);
 extern struct bst_test_list *test_adv_install(struct bst_test_list *test);
+extern struct bst_test_list *test_blob_install(struct bst_test_list *test);
 #endif
 
 bst_test_install_t test_installers[] = {
@@ -46,6 +47,7 @@ bst_test_install_t test_installers[] = {
 	test_access_install,
 	test_ivi_install,
 	test_adv_install,
+	test_blob_install,
 #endif
 	NULL
 };
