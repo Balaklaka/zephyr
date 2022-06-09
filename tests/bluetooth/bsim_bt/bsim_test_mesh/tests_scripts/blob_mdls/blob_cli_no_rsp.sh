1c4343d887898f639f57f784160f2902292347c5
Anders Storrø <anders.storro@nordicsemi.no>
2022-06-09 13:47:51 +0200

    Tests: Bluetooth: Mesh: BLOB cli no response
    - Tests that BLOB Client suspends if no target responds to Block get.
    - Tests that BLOB Client transfer ends if no target responds to Xfer
      get.
    
    Signed-off-by: Anders Storrø <anders.storro@nordicsemi.no>

---
+++ tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/blob_mdls/blob_cli_no_rsp.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+RunTest mesh_blob \
+	blob_cli_fail_on_no_rsp \
+	blob_srv_fail_on_block_get \
+	blob_srv_fail_on_block_get -- -argstest msg-fail-type=0
+
+RunTest mesh_blob \
+	blob_cli_fail_on_no_rsp \
+	blob_srv_fail_on_xfer_get \
+	blob_srv_fail_on_xfer_get -- -argstest msg-fail-type=1
