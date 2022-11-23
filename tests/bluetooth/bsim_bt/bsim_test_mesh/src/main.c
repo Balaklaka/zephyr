5bc7a43c801c769ec545726ba4d5e6076e7ce6f8
Stine Akredalen <stine.akredalen@nordicsemi.no>
2022-11-23 05:27:08 -0800

    tests: Bluetooth: Mesh: added SAR stress test
    Test that a maximum length SDU can be processed with SAR
    with "stressed" transmitter and receiver configurations.
    
    Signed-off-by: Stine Akredalen <stine.akredalen@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_mesh/src/main.c
+++ tests/bluetooth/bsim_bt/bsim_test_mesh/src/main.c
@@ -29,6 +29,7 @@ extern struct bst_test_list *test_ivi_install(struct bst_test_list *test);
 extern struct bst_test_list *test_adv_install(struct bst_test_list *test);
 extern struct bst_test_list *test_blob_install(struct bst_test_list *test);
 extern struct bst_test_list *test_op_agg_install(struct bst_test_list *test);
+extern struct bst_test_list *test_sar_install(struct bst_test_list *test);
 #endif
 
 bst_test_install_t test_installers[] = {
@@ -54,6 +55,7 @@ bst_test_install_t test_installers[] = {
 	test_adv_install,
 	test_blob_install,
 	test_op_agg_install,
+	test_sar_install,
 #endif
 	NULL
 };
